e198532b41cae707914f173151e1cca3795bb4bc
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-05-24 12:56:10 +0530

    added home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Sender from "./pages/Sender.jsx";
 import Receiver from "./pages/Receiver.jsx";
+import Home from "./pages/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Router>
         <Routes>
             <Route path="" element={<Sender />}></Route>
             <Route path="receiver/:connId?" element={<Receiver />} />
+            <Route path="home" element={<Home />} />
         </Routes>
     </Router>
 );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                accent: "#1A3329",
+                highlight: "#336654",
+                background: "#12211C",
+                button_heighlight: "#0FD18C",
+            },
+        },
     },
     plugins: [],
 };
